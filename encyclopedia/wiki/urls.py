29e17963be3ef038b463989be3b94e4f3bff3380
astafanous <astafanousf@yahoo.com>
2021-07-01 13:43:13 +0200

    Add index, edit, page HTML FILE

--- encyclopedia/wiki/urls.py
+++ encyclopedia/wiki/urls.py
@@ -6,4 +6,6 @@ app_name = 'wiki'
 
 urlpatterns = [
      path('', views.index, name="index"),
+     path("edit", views.edit, name="edit"),
+     path("<str:title>", views.page, name="page"),
 ]
--- encyclopedia/wiki/views.py
+++ encyclopedia/wiki/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from . import util
+
 
 # Create your views here.
+
+def index(request):
+    
+    return render(request, "wiki/index.html", {
+        "entries": util.list_entries()
+        
+        
+    })
+
+def page(request, title):
+   
+    return render(request, "wiki/page.html", {
+        "entries": util.get_entry(title)
+        
+    })
+
+def edit(request):
+    return render(request, "wiki/edit.html") 
